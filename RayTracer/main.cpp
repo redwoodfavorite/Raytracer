75e38073d65a584de67a369f1be9d13d3e68a9b2
Joseph Sample <joseph.michael.sample@gmail.com>
2015-11-25 18:06:53 -0800

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Joseph Sample on 11/25/15.
+//  Copyright © 2015 Joseph Sample. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
